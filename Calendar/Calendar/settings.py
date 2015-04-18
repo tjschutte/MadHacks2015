60183cd0258ec4c6f22155a20edb133ce08b1f06
AbdrodeDomeris <aklapper4@gmail.com>
2015-04-18 15:50:11 -0500

    Basic Data Structure

--- Calendar/Calendar/settings.py
+++ Calendar/Calendar/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taskmaster'
 )
 
 MIDDLEWARE_CLASSES = (
--- Calendar/taskmaster/models.py
+++ Calendar/taskmaster/models.py
@@ -1,3 +1,44 @@
 from django.db import models
-
+import datetime
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
+class Task(models.Model):
+    dueDate = models.DateTimeField('Due Date', null = True)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
+    category = models.ForeignKey('Category')
+
+class Category(models.Model):
+    name = models.CharField(primary_key=True, max_length=100)
+    color = models.ForeignKey('Color')
+
+class Event(models.Model):
+    startDate = models.DateTimeField('Start')
+    endDate = models.DateTimeField('End')
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
+    category = models.ForeignKey('Category')
+    
+class Color(models.Model):
+    R = models.IntegerField(
+        default=0,
+        validators=[
+            MaxValueValidator(255),
+            MinValueValidator(0)
+        ]
+     )
+    G = models.IntegerField(
+        default=0,
+        validators=[
+            MaxValueValidator(255),
+            MinValueValidator(0)
+        ]
+     )
+    B = models.IntegerField(
+        default=0,
+        validators=[
+            MaxValueValidator(255),
+            MinValueValidator(0)
+        ]
+     )
